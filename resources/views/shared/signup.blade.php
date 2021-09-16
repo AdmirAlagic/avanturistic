d05be490fd02a9eca0350af9224aafc8bea0f282
Admir <admir@omnitask.ba>
2021-09-16 14:26:08 +0200

    signup

--- resources/views/shared/signup.blade.php
+++ resources/views/shared/signup.blade.php
@@ -25,7 +25,7 @@
                             {!! Form::open(['url' => 'register', 'method' => 'POST', 'class' => 'kt-form', 'novalidate' => 'novalidate', 'style' => 'padding:15px;padding-top:0;padding-bottom:5px;', 'id' => 'signup-form']) !!}
                             <div class="row class-text-center">
                                 <div class="col-12 ">
-                                    
+                                    <div id="signup-errors" style="margin-top:10px;"></div>
                                     <!--begin::Form-->
                                     <div class="form-group">
                                         {!! Form::text('email', null, ['class' => 'auth-input form-control', 'placeholder' => 'Email', ]) !!}
@@ -46,20 +46,23 @@
                                
                             </div>
 
+                           <div class="form-group">
                             <button id="signupSubmit" class="btn btn-green" style="padding: 5px;position:relative;-webkit-border-radius: 4px !important;-moz-border-radius: 4px !important;border-radius: 4px !important; width:100%;height:35px;">
-                            <span style="position:absolute;right:35%;top:15px;display:none;" class="kt-spinner  kt-spinner--sm kt-spinner--light signupLoading" ></span> <span class="text-white signupSubmit"><b>Sign Up</b></span>
-                            </button>
-                           
+                                <span style="position:absolute;right:35%;top:15px;display:none;" class="kt-spinner  kt-spinner--sm kt-spinner--light signupLoading" ></span> <span class="text-white signupSubmit"><b>Sign Up</b></span>
+                                </button>
+                               
+                                <p class="mt-10 text-gray" style="line-height:1rem; font-weight:300; ">
+                                    <small> By signing up, you agree to our  
+                                        <a class="text-muted" href="/terms-and-conditions">terms</a> &
+                                        <a class="text-muted" href="/privacy-policy">privacy policy</a>.</small>
+                                </p>
+                           </div>
                              
-                            <div id="signup-errors" style="margin-top:10px;"></div>
+                           
                             {!! Form::close() !!}
 
 
-                            <p style="color:#999; line-height:1rem; ">
-                                <small> By signing up, you agree to our  
-                                    <a class="text-muted" href="/terms-and-conditions">terms</a> &
-                                    <a class="text-muted" href="/privacy-policy">privacy policy</a>.</small>
-                            </p>
+                           
                            {{--  <p style="padding-top:0px;">
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 20 24" version="1.1" class="kt-svg-icon">
                                     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
