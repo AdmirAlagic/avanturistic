e3a52494c07072acd11b8fa763a786b83a64f8c8
Admir <admir@omnitask.ba>
2021-09-22 23:51:00 +0200

    profiles

--- resources/views/profile/business.blade.php
+++ resources/views/profile/business.blade.php
@@ -149,7 +149,7 @@
                                                         </div>
                                                     @endif
 
-                                                    <a class="btn btn-default mt-10 mb-10" href="/message/{{ $model->id }}">
+                                                    <a class="btn btn-line-rounded mt-10 mb-10" href="/message/{{ $model->id }}">
                                                         <svg xmlns="http://www.w3.org/2000/svg"  style="width:18px;" fill="none" viewBox="0 0 24 24" stroke="#474747">
                                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                                           </svg>
