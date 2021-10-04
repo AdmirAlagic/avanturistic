06ac961eb098aa01627e629fcd8937d49cd6aac5
Admir <admir@omnitask.ba>
2021-10-04 23:32:05 +0200

    post share

--- resources/views/shared/post/post_share.blade.php
+++ resources/views/shared/post/post_share.blade.php
@@ -1,6 +1,6 @@
 <div class="flex justify-end">
     <a  href="#" data-post_id="{{ $post->id }}" id="likeBtn" class="btn post-toolbar-btn  flex flex-col items-center mr-10 ">
-        <i style="width: 36px;height: 36px; -webkit-border-radius: 99999em;-moz-border-radius: 99999em;border-radius: 99999em; padding-top: 19px;padding-left: 6px;background: #474747; "  class="fa fa-heart {{ $alreadyLiked ? 'text-success' : 'text-white' }}"></i>
+        <i style="width: 36px;height: 36px; -webkit-border-radius: 99999em;-moz-border-radius: 99999em;border-radius: 99999em; padding-top: 19px;padding-left: 6px;background: #474747;font-size: 1.5em !important; "  class="fa fa-heart {{ $alreadyLiked ? 'text-success' : 'text-white' }}"></i>
         <span style="margin-left: -3px;" class="text-muted" id="likesCount">{{ $post->likes }}</span>
     </a>
     <a  data-toggle="kt-tooltip" title="" data-placement="right" data-original-title="I Was Here!" href="#" data-post_id="{{ $post->id }}" id="visitedBtn"  class="btn post-toolbar-btn flex flex-col items-center ">
